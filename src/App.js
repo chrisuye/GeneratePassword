a04cfbe195536ddda65f11629d42c2043951e6f4
Christian Seyoum <chriscoding96@gmail.com>
2021-08-28 18:53:24 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Slider from 'react-input-slider';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(9);
+  const [password, setPassword] = useState('');
+  
+
+  const lowercase = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  const numbers = '0123456789'.split('');
+  const symbols = '!@#$%^&*()'.split('');
+  const [options, setOptions] = useState([...lowercase]);
+
+  const editOptions = (index) => {
+    if (index === 1) {
+      if (options.includes(...uppercase)) {
+        setOptions(options.filter(item => !uppercase.includes(item)));
+      } else {
+        setOptions(options.concat(...uppercase));
+      }
+    }
+    if (index === 2) {
+      if (options.includes(...numbers)) {
+        setOptions(options.filter(option => !numbers.includes(option)));
+      } else {
+        setOptions(options.concat(...numbers));
+      }
+    }
+    if (index === 3) {
+      if (options.includes(...symbols)) {
+        setOptions(options.filter(option => !symbols.includes(option)));
+      } else {
+        setOptions(options.concat(...symbols));
+      }
+    }
+  }
+
+  const generatePassword = () => {
+    let password = '';
+
+    for (let i = 0; i < count; i++) {
+      const randomOptionIndex = Math.floor(Math.random() * options.length);
+      password += options[randomOptionIndex];
+    }
+    setPassword(password);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='inner'>
+        <h1>Password Generator</h1>
+        {password !== '' ?
+        <div className='password'>
+        <p>{password}</p>
+        <button onClick={() => {navigator.clipboard.writeText(password)}}>Copy</button>
+      </div>: null}
+        <div className='slide'>
+          <p>Length: {count}</p>
+          <Slider
+            axis="x"
+            x={count}
+            xmin={9}
+            xmax={20}
+            onChange={({ x }) => setCount(x)}
+          />
+        </div>
+        <div className='options'>
+          <div className='option' onClick={() => editOptions(1)}>
+            <p> <input type='checkbox' checked={options.includes(...uppercase)}/> Uppercase Letters</p>
+          </div>
+          <div className='option' onClick={() => editOptions(2)}>
+            <p><input type='checkbox' checked={options.includes(...numbers)}/>Numbers</p>
+          </div>
+          <div className='option' onClick={() => editOptions(3)}>
+            <p><input type='checkbox' checked={options.includes(...symbols)}/>Symbols</p>
+          </div>
+        </div>
+        <div className='generate'>
+          <button onClick={() => generatePassword()}>Generate</button>
+        </div>
+        </div>
     </div>
   );
 }
